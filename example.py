008e2211ac77defd02336f3e8546dd16072f8408
NemoDev <82635729+semen-bol@users.noreply.github.com>
2024-10-11 23:52:25 +0500

    Update and rename main.py to example.py

--- example.py
+++ example.py
@@ -3,7 +3,5 @@ from screenShot import getScreenShot
 screen = getScreenShot()
 
 print(screen.getScreenWebSite())
-print(screen.getScreenWebSite())
-print(screen.getScreenWebSite())
 
 screen.quitSession()
