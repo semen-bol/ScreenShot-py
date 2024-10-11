b507f8702eb2037c0af4ff5375b95e6ad24c6885
NemoDev <82635729+semen-bol@users.noreply.github.com>
2024-10-12 03:25:38 +0500

    Update example.py

--- example.py
+++ example.py
@@ -3,5 +3,7 @@ from screenShot import getScreenShot
 screen = getScreenShot()
 
 print(screen.getScreenWebSite())
+# or get your custom url
+print(screen.getScreenWebSite(url = "https://vk.ru/"))
 
 screen.quitSession()
